e1a65ad795828cd600d38ffd2377ff9cb70a5c30
Rob Golshan <rpgolshan@gmail.com>
2017-02-12 16:34:42 -0600

    q1.a done. Cyclicbarrier implemented with a semaphore

--- hw2/q1/CyclicBarrier.java
+++ hw2/q1/CyclicBarrier.java
@@ -1,18 +1,27 @@
 /*
- * EID's of group members
- * 
+ * Robert Golshan, rpg499
+ * Jonathan Friesen, jtf698 
  */
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CyclicBarrier {
+    private int parties;
+    private AtomicInteger numWaiting = new AtomicInteger(0);
+    Semaphore s = new Semaphore(0, true);
 	
 	public CyclicBarrier(int parties) {
+        this.parties = parties;
 	}
 	
 	public int await() throws InterruptedException {
-           int index = 0;
-		
-          // you need to write this code
-	    return index;
+        int index = parties -1 - numWaiting.getAndIncrement();
+        if (index == 0) {
+            s.release(numWaiting.getAndSet(0) - 1);
+        }
+        else {
+            s.acquire();
+        }
+        return index;
 	}
 }
