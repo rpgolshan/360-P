31a5108832f08b67278bf445f374f78d38cfc4d8
Rob Golshan <rpgolshan@gmail.com>
2017-02-12 18:55:29 -0600

    q2 done

--- hw2/q2/FairReadWriteLock.java
+++ hw2/q2/FairReadWriteLock.java
@@ -1,15 +1,47 @@
+import java.util.PriorityQueue;
+
 public class FairReadWriteLock {
-	
+    private int numWriters = 0;
+    private int numReaders = 0;
+
+    PriorityQueue<Thread> threads = new PriorityQueue<Thread>();
+    public FairReadWriteLock() {
+    
+    }
                         
-	public synchronized void beginRead() {
+	public synchronized void beginRead() throws InterruptedException  {
+        threads.add(Thread.currentThread());
+
+        while (threads.peek() != Thread.currentThread()) {
+            wait();
+        }
+        while (numWriters > 0) {
+            wait();
+        }
+        numReaders++;
+        threads.poll();
 	}
 	
 	public synchronized void endRead() {
+        numReaders--;
+        notifyAll();
 	}
 	
-	public synchronized void beginWrite() {
+	public synchronized void beginWrite()  throws InterruptedException{
+        threads.add(Thread.currentThread());
+        while (threads.peek() != Thread.currentThread()) {
+            wait();
+        }
+        
+        while (numReaders > 0 || numWriters > 0) {
+            wait(); 
+        }
+        numWriters++;
+        threads.poll();
 	}
 	public synchronized void endWrite() {
+        numWriters--;
+        notifyAll();
 	}
 }
 	
