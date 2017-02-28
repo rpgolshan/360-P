7d6bd392a3f72e0730ac181d11c470fe935fd316
Rob Golshan <rpgolshan@gmail.com>
2017-02-28 14:34:44 -0600

    Server parsing of original inventory implmented

--- hw3/q3/Server.java
+++ hw3/q3/Server.java
@@ -1,4 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Hashtable;
+
 public class Server {
+    private Hashtable<String, Integer> ht = new Hashtable<String, Integer>();
+    public Server() {
+    } 
+
+    public int increment(String product) {
+        int current = ht.get(product); 
+        current++;
+        return ht.put(product, current);
+    }
+    public int set(String product, int amount) {
+        return ht.put(product, amount); 
+    }
+
+    public int get(String product) {
+        return ht.get(product);  
+    }
+
   public static void main (String[] args) {
     int tcpPort;
     int udpPort;
@@ -14,8 +35,25 @@ public class Server {
     udpPort = Integer.parseInt(args[1]);
     String fileName = args[2];
 
+    Server ser = new Server();
     // parse the inventory file
 
+    try {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] s = line.split(" ");
+            if (s.length < 2) { 
+                System.out.println("Incorrect ammount of variables");
+                System.exit(1);
+            }
+            ser.set(s[0], Integer.parseInt(s[1]));
+        }
+        reader.close();
+    } catch(Exception e){
+        e.printStackTrace();
+    }
     // TODO: handle request from clients
+    
   }
 }
